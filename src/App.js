29550b0543ef6b7a239278562db7cf231955af73
amirfaraji78 <amirhoseinfaraji784@gmail.com>
2022-03-27 01:57:09 +0430

    fixed wrong erorr

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from"react";
+import React  from"react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home"
 import CharacterDetails from  "./Components/ComponentsMarket/Character/CharacterDetails.js";
