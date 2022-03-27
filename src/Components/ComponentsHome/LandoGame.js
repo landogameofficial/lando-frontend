ea903ceec1b4286f9bfb265c5fc51c19155491f2
amirfaraji78 <amirhoseinfaraji784@gmail.com>
2022-03-27 21:23:37 +0430

    add play game page

---
+++ src/Components/ComponentsHome/LandoGame.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./LandoGame.css";
+class LandoGame extends React.Component {
+
+  render() {
+    return (
+      <div className="game-container">
+        <div className="header-game">This game is a Lando game demo</div>
+        <div>
+          <object
+            aria-labelledby="label1"
+            className="lando-game"
+            type="text/html"
+            data="https://venge.io"
+          ></object>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LandoGame;
