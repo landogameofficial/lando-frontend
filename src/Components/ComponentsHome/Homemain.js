d4569726636332fef6e20b3de033d06b2c8253ed
amirfaraji78 <amirhoseinfaraji784@gmail.com>
2022-04-06 17:43:48 +0430

    change paragraph

--- src/Components/ComponentsHome/Homemain.js
+++ src/Components/ComponentsHome/Homemain.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Jump from "react-reveal/Jump";
 import "./Homemain.css";
 import mainImag from "./../../assets/mainImage1.png";
@@ -14,32 +14,57 @@ const Homemain = () => {
   return (
     <>
       <div className="homemain-container">
-        
         <div className="social-bar">
-            <Jump>
-            <a href="https://github.com/landogameofficial" rel="noreferrer"target="_blank" className="social-icon-link"><i class='fab fa-github' /></a>
-            <a href="https://www.youtube.com/channel/UCkf0baLkUqnvRQVXWJ1766Q" rel="noreferrer" target="_blank" className="social-icon-link"><i class='fab fa-youtube' /></a>
-            <a href="https://twitter.com/Lando_game" rel="noreferrer" target="_blank" className="social-icon-link"><i class='fab fa-twitter' /></a>
-            <a href="https://t.me/landogame" rel="noreferrer" target="_blank" className="social-icon-link"><i class='fab fa-telegram-plane' /></a>
-            </Jump>
+          <Jump>
+            <a
+              href="https://github.com/landogameofficial"
+              rel="noreferrer"
+              target="_blank"
+              className="social-icon-link"
+            >
+              <i class="fab fa-github" />
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UCkf0baLkUqnvRQVXWJ1766Q"
+              rel="noreferrer"
+              target="_blank"
+              className="social-icon-link"
+            >
+              <i class="fab fa-youtube" />
+            </a>
+            <a
+              href="https://twitter.com/Lando_game"
+              rel="noreferrer"
+              target="_blank"
+              className="social-icon-link"
+            >
+              <i class="fab fa-twitter" />
+            </a>
+            <a
+              href="https://t.me/landogame"
+              rel="noreferrer"
+              target="_blank"
+              className="social-icon-link"
+            >
+              <i class="fab fa-telegram-plane" />
+            </a>
+          </Jump>
         </div>
-        
-       
 
         <div>
           <div>
             <h1 className="text-h">
-              <b>Vega Game</b> is a play to earn metaverse game platform on
-              Polygon network.
+              <b>Lando </b> is a play to earn metaverse game on Binance Smart
+              Chain platform.
             </h1>
           </div>
 
           <div>
             <div className="text-p">
               <p1>
-                This game is first game shooter on blockchain and store data
-                with oracle and designed based on skill, and your speed of
-                action
+                Blockchain elements in this game are most often based on the use
+                of cryptocurrency or non-fungible tokens (NFTs) which players
+                can buy, earn, or trade with other players.
               </p1>
             </div>
 
@@ -54,9 +79,9 @@ const Homemain = () => {
                 </p1>
               </div>
               <Link className="link-game" target="_blanke" to="/playgame">
-              <div className="btn-contract btn-game">
-                <p1>Play Game</p1>
-              </div>
+                <div className="btn-contract btn-game">
+                  <p1>Play Game</p1>
+                </div>
               </Link>
             </div>
           </div>
