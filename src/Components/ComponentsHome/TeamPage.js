718b7169c3e1dafee39ab94253e79e5684bcf221
amirfaraji78 <amirhoseinfaraji784@gmail.com>
2022-04-07 15:53:26 +0430

    fixed error

--- src/Components/ComponentsHome/TeamPage.js
+++ src/Components/ComponentsHome/TeamPage.js
@@ -4,7 +4,7 @@ const TeamPage = ({ name, img, position, social, socialLogo }) => {
   return (
     <>
       <div className="team">
-        <img src={img} className="image" alt="Team image" />
+        <img src={img} className="image" alt="Team" />
         <p1 className="name-team">{name}</p1>
         <p1 className="name-position">{position}</p1>
         <a
