2770a271dc5795f24d2a7e3493516c8bdc28c7b5
amirfaraji78 <amirhoseinfaraji784@gmail.com>
2022-04-08 18:15:06 +0430

    fixed error

--- src/Components/ComponentsHome/Partners.js
+++ src/Components/ComponentsHome/Partners.js
@@ -20,16 +20,16 @@ const Partners = () => {
           <h1 className="partners-header">Partners</h1>
       </Fade>
       <div className="partners-container">
-        <LightSpeed><a href="https://www.blockchain.capital/" target="_blank" ><img src={partnerimage1} alt="partner image1"/></a></LightSpeed>
-        <LightSpeed><a href="https://www.kaleido.io/" target="_blank"><img src={partnerimage2} alt="partner image2" /></a></LightSpeed>
-        <LightSpeed><a href="https://www.8dcapital.com/" target="_blank"><img src={partnerimage3} alt="partner image3" /></a></LightSpeed>
-        <LightSpeed><a href="https://www.hashversecapital.com/" target="_blank"><img src={partnerimage4} alt="partner image4" /></a></LightSpeed>
-        <LightSpeed><a href="https://www.saheelcapital.io/" target="_blank"><img src={partnerimage5} alt="partner image5" /></a></LightSpeed>
-        <LightSpeed><a href="https://www.exan.tech/" target="_blank"><img src={partnerimage6} alt="partner image6" /></a></LightSpeed>
-        <LightSpeed><a href="https://www.stasis.net/" target="_blank"><img src={partnerimage7} alt="partner image7" /></a></LightSpeed>
-        <LightSpeed><a href="https://www.x21digital.com/" target="_blank"><img src={partnerimage8} alt="partner image8" /></a></LightSpeed>
-        <LightSpeed><a href="https://www.beetrootlab.com/" target="_blank"><img src={partnerimage9} alt="partner image9" /></a></LightSpeed>
-        <LightSpeed><a href="https://www.aminocapital.com/" target="_blank"><img src={partnerimage10} alt="partner image10" /></a></LightSpeed>
+        <LightSpeed><a href="https://www.blockchain.capital/" target="_blank" rel="noreferrer" ><img src={partnerimage1} alt="partner image1"/></a></LightSpeed>
+        <LightSpeed><a href="https://www.kaleido.io/" target="_blank" rel="noreferrer" ><img src={partnerimage2} alt="partner image2" /></a></LightSpeed>
+        <LightSpeed><a href="https://www.8dcapital.com/" target="_blank" rel="noreferrer" ><img src={partnerimage3} alt="partner image3" /></a></LightSpeed>
+        <LightSpeed><a href="https://www.hashversecapital.com/" target="_blank" rel="noreferrer" ><img src={partnerimage4} alt="partner image4" /></a></LightSpeed>
+        <LightSpeed><a href="https://www.saheelcapital.io/" target="_blank" rel="noreferrer" ><img src={partnerimage5} alt="partner image5" /></a></LightSpeed>
+        <LightSpeed><a href="https://www.exan.tech/" target="_blank" rel="noreferrer" ><img src={partnerimage6} alt="partner image6" /></a></LightSpeed>
+        <LightSpeed><a href="https://www.stasis.net/" target="_blank" rel="noreferrer"  ><img src={partnerimage7} alt="partner image7" /></a></LightSpeed>
+        <LightSpeed><a href="https://www.x21digital.com/" target="_blank" rel="noreferrer" ><img src={partnerimage8} alt="partner image8" /></a></LightSpeed>
+        <LightSpeed><a href="https://www.beetrootlab.com/" target="_blank" rel="noreferrer" ><img src={partnerimage9} alt="partner image9" /></a></LightSpeed>
+        <LightSpeed><a href="https://www.aminocapital.com/" target="_blank"rel="noreferrer" ><img src={partnerimage10} alt="partner image10" /></a></LightSpeed>
         
         
         
