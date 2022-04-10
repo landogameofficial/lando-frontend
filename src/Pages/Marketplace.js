c67a638c6937df24b2ff8670bdb51068a5f5cc64
amirfaraji78 <amirhoseinfaraji784@gmail.com>
2022-04-10 23:14:08 +0430

    delete extra file

--- src/Pages/Marketplace.js
+++ src/Pages/Marketplace.js
@@ -1,9 +1,7 @@
 import React from "react";
-
 import Character from "../Components/ComponentsMarket/Character/Character";
 import Gun from "../Components/ComponentsMarket/Gun/Gun";
 import Header from "../Components/ComponentsMarket/Header";
-import "./Marketplace.css"
 import Handle from "../Components/ComponentsMarket/Handle/Handle";
 import Skill from "../Components/ComponentsMarket/Skill/Skill";
 const Marketplace = () => {
